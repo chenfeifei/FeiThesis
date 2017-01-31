59a8830aef9b9c40837411a864fbd35543b0c3b7
chenfeifei <chenfei.feifeifei@hotmail.com>
2017-01-31 11:06:18 -0800

    version 2017-01-31

---
+++ visualization/chapter5_array_visualization.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+# Target Composition
+dataArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.299586, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0.664053, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.021196, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0.00662804, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.000642609, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.00789, 0, 0, 0, 0, 0]]
+'''
+# Return Composition of E1
+dataArray = [[0, 0, 0, 0, 0, 0, 0.299586, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0.664053, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0.021196, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0.00662804, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0.000642609, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0.00789, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+# Return Compostion of E6
+dataArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.299586, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0.664053, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.021196, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0.00662804, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.000642609, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.00789, 0, 0, 0, 0, 0]]
+'''
+plt.xticks(range(1,18))
+ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0, vmax=1, linewidths=.5, fmt="g")
+ax.set_xticklabels([r'$0^{\circ}$', r'$10^{\circ}$', r'$20^{\circ}$', r'$30^{\circ}$', r'$40^{\circ}$', r'$50^{\circ}$', r'$60^{\circ}$', r'$70^{\circ}$', r'$80^{\circ}$', r'$100^{\circ}$', r'$110^{\circ}$', r'$120^{\circ}$', r'$130^{\circ}$', r'$140^{\circ}$', r'$150^{\circ}$', r'$160^{\circ}$', r'$170^{\circ}$', r'$180^{\circ}$'])
+ax.set_yticklabels(['Methionine', 'Leucine', 'Isoleucine', 'Alanine', 'Threonine', 'Valine'])
+ax.set_xlabel(r'$\theta$' " Vaule")
+ax.set_ylabel("Amino Acids")
+ax.set_title("Target Composition for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
+#ax.set_title("Return Composition of E1 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
+#ax.set_title("Return Composition of E6 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
+
+plt.show()
+
+
