a1b9c2809df9a521e69dea46755757cdc028a324
chenfeifei <chenfei.feifeifei@hotmail.com>
2017-02-07 18:43:17 -0800

    Chapter 6 update and heatmap update

--- visualization/chapter6_array_visualization.py
+++ visualization/chapter6_array_visualization.py
@@ -20,22 +20,24 @@ dataArray = [[0, 0, 0, 0, 0, 0, 0.299586, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0.00789, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 '''
 # Return Compostion of E6
-dataArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.299586, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0.664053, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.021196, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0.00662804, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.000642609, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.00789, 0, 0, 0, 0, 0]]
+dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.019308, np.nan],
+[np.nan, 0.443574, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+[np.nan, np.nan, np.nan, 0.11847, np.nan, np.nan, np.nan, np.nan, np.nan],
+[0.001038, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
+[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.010914, np.nan],
+[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.006696, np.nan, np.nan],
+[0.4, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
 
-plt.xticks(range(1,18))
-ax = sns.heatmap(dataArray,annot=False,square=True, cmap="Reds", vmin=0, vmax=1, linewidths=.5, fmt="g")
-ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170, 180])
-ax.set_yticklabels(['Methionine', 'Leucine', 'Isoleucine', 'Alanine', 'Threonine', 'Valine'])
+plt.xticks(range(1,9))
+ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0, vmax=1, linewidths=.5, fmt="g")
+#ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170, 180])
+ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80])
+ax.set_yticklabels(['Valine', 'Threonine', 'Alanine', 'Isoleucine', 'Leucine', 'Methionine'])
 ax.set_xlabel(r'$\theta$' " Vaule")
 ax.set_ylabel("Amino Acids")
 #ax.set_title("Target Composition for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return Composition of E1 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
-ax.set_title("Return Composition of E6 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
+ax.set_title("Result composition of Experiment 2 in one random run with experiments containing scaling factor of mixed amino acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$80^{\circ}$')
 
 plt.show()
 
