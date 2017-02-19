6799b5412563c3fbf788d737e5741efd107ccb49
chenfeifei <chenfei.feifeifei@hotmail.com>
2017-02-18 16:56:20 -0800

    chapter 5 and 6 heatmap figures update

--- visualization/chapter5_array_visualization.py
+++ visualization/chapter5_array_visualization.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib.colors import ListedColormap
 
-'''
+
 # Target Composition
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.299586, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, 0.664053, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
@@ -12,6 +12,7 @@ dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.000642609, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.00789, np.nan, np.nan, np.nan, np.nan, np.nan]]
 
+'''
 # Return Composition of E2
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.299586, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, 0.664053, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
@@ -20,6 +21,7 @@ dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.299586, np.nan, 
 [np.nan, np.nan, np.nan, np.nan, np.nan, 0.000642609, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, 0.00789, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
 '''
+'''
 # Return Compostion of E6
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.299586, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, 0.664053, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
@@ -27,17 +29,17 @@ dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.00662804, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.000642609, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.00789, np.nan, np.nan, np.nan, np.nan, np.nan]]
-
+'''
 plt.xticks(range(1,18))
-ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0.000001, vmax=1, linewidths=.5, fmt="g",cbar=False)
-ax.set_xticklabels([r'$0^{\circ}$', r'$10^{\circ}$', r'$20^{\circ}$', r'$30^{\circ}$', r'$40^{\circ}$', r'$50^{\circ}$', r'$60^{\circ}$', r'$70^{\circ}$', r'$80^{\circ}$', r'$100^{\circ}$', r'$110^{\circ}$', r'$120^{\circ}$', r'$130^{\circ}$', r'$140^{\circ}$', r'$150^{\circ}$', r'$160^{\circ}$', r'$170^{\circ}$', r'$180^{\circ}$'])
-ax.set_yticklabels(['Valine', 'Threonine', 'Alanine', 'Isoleucine', 'Leucine', 'Methionine'])
+ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0.000001, vmax=1, linewidths=.5, fmt=".2g",cbar=False, annot_kws={"size": 8})
+ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170, 180])
+ax.set_yticklabels(['Val', 'Thr', 'Ala', 'Ile', 'Leu', 'Met'], rotation=0)
 ax.set_xlabel(r'$\theta$' " Vaule")
 ax.set_ylabel("Amino Acids")
 #ax.set_title("Target Composition for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return Composition of E1 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return Composition of E6 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
-
+#ax.set_xticklabels([r'$0^{\circ}$', r'$10^{\circ}$', r'$20^{\circ}$', r'$30^{\circ}$', r'$40^{\circ}$', r'$50^{\circ}$', r'$60^{\circ}$', r'$70^{\circ}$', r'$80^{\circ}$', r'$100^{\circ}$', r'$110^{\circ}$', r'$120^{\circ}$', r'$130^{\circ}$', r'$140^{\circ}$', r'$150^{\circ}$', r'$160^{\circ}$', r'$170^{\circ}$', r'$180^{\circ}$'])
 plt.show()
 
 
--- visualization/chapter6_array_visualization.py
+++ visualization/chapter6_array_visualization.py
@@ -2,14 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-
-# Return Compostion of E6
+'''
+# Target Compostion 
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.03218, np.nan],
 [np.nan, 0.73929, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, 0.19745, np.nan, np.nan, np.nan, np.nan, np.nan],
 [0.00173, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.01819, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.01116, np.nan, np.nan]]
+'''
+'''
+# Return Composition 
+dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.019308, np.nan],
+[np.nan, 0.443574, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
+[np.nan, np.nan, np.nan, 0.11847, np.nan, np.nan, np.nan, np.nan, np.nan],  
+[0.001038, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
+[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.010914, np.nan], 
+[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.006696, np.nan, np.nan], 
+[0.4, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
+'''
 
 '''
 #chapter6_figure_three.png
@@ -20,31 +31,34 @@ dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, 0.414239, np.nan, np.nan, 
 [np.nan, np.nan, np.nan, np.nan, 0.0123589, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
 [np.nan, np.nan, 0.0185461, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
 [0.229495, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
-
-
+'''
+'''
+#chapter6_figure_four.png
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, 0.27162, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.142619, np.nan, np.nan, np.nan, np.nan, np.nan], [np.nan, np.nan, 0.135875, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.0713442, np.nan, np.nan], 
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.0104812, np.nan, np.nan, 0.0199615, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
 [np.nan, np.nan, 0.0301941, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.0575048, np.nan, np.nan], 
 [np.nan, np.nan, np.nan, np.nan, 0.00810383, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.00425508, np.nan, np.nan, np.nan, np.nan], 
 [np.nan, np.nan, 0.0121608, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.00638527, np.nan, np.nan], 
 [0.229495, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan,np.nan, np.nan, np.nan, np.nan, np.nan]]
+'''
 
-
+#chapter6_figure_five.png
 dataArray = [[np.nan, np.nan, np.nan, np.nan, np.nan, 0.53762, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, 0.26894, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.03951, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], 
 [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 0.11382, np.nan, np.nan], 
 [np.nan, np.nan, np.nan, np.nan, 0.01604, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
 [np.nan, np.nan, 0.02407, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
-'''
 
-plt.xticks(range(1,9))
-ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0, vmax=1, linewidths=.5, fmt="g",cbar=False)
+
+plt.xticks(range(1,18))
+ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0, vmax=1, linewidths=.5, fmt=".2g",cbar=False, annot_kws={"size": 8})
 ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170, 180])
 #ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80])
-ax.set_yticklabels(['Valine', 'Threonine', 'Alanine', 'Isoleucine', 'Leucine', 'Methionine'])
+ax.set_yticklabels(['Val', 'Thr', 'Ala', 'Ile', 'Leu', 'Met'], rotation=0)
 ax.set_xlabel(r'$\theta$' " Vaule")
 ax.set_ylabel("Amino Acids")
+
 #ax.set_title("Target Composition for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return Composition of E1 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return composition of Experiment 2 in one random run with experiments containing scaling factor and the mixed amino acids' candidates with " r'$\theta$' " expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
