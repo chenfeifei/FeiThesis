64336f5cd8b8cc65f57793ac07bb11ff7d3f1347
chenfeifei <chenfei.feifeifei@hotmail.com>
2017-03-03 18:16:24 -0800

    chapter 6 and figures update Dennis comment

--- visualization/chapter6_array_visualization.py
+++ visualization/chapter6_array_visualization.py
@@ -56,13 +56,13 @@ ax = sns.heatmap(dataArray,annot=True,square=True, cmap="Reds", vmin=0, vmax=1, 
 ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170, 180])
 #ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80])
 ax.set_yticklabels(['Val', 'Thr', 'Ala', 'Ile', 'Leu', 'Met'], rotation=0)
-ax.set_xlabel(r'$\theta$' " Vaule")
+ax.set_xlabel(r'tilt' " Angle")
 ax.set_ylabel("Amino Acids")
 
 #ax.set_title("Target Composition for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return Composition of E1 for One Run of Mixed Amino Acids with " r'$\theta$' " Expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 #ax.set_title("Return composition of Experiment 2 in one random run with experiments containing scaling factor and the mixed amino acids' candidates with " r'$\theta$' " expanded from " r'$0^{\circ}$' " to " r'$180^{\circ}$')
 
-plt.show()
+plt.savefig('chapter6_figure_five.png', dpi=100)
 
 
